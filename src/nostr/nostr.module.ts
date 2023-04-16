36a65aaa9453e96229bc7759c2af9d733b857617
codytseng <codytseng@outlook.com>
2023-04-16 11:26:49 +0800

    fix: missing LockService provider

--- src/nostr/nostr.module.ts
+++ src/nostr/nostr.module.ts
@@ -3,11 +3,12 @@ import { MongoRepositoriesModule } from './mongo-repositories/mongo-repositories
 import { NostrController } from './nostr.controller';
 import { NostrGateway } from './nostr.gateway';
 import { EventService } from './services/event.service';
+import { LockService } from './services/lock.service';
 import { SubscriptionService } from './services/subscription.service';
 
 @Module({
   imports: [MongoRepositoriesModule],
   controllers: [NostrController],
-  providers: [NostrGateway, SubscriptionService, EventService],
+  providers: [NostrGateway, SubscriptionService, EventService, LockService],
 })
 export class NostrModule {}
