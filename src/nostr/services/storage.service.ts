91ef9205d8861b884e62c9fa76d357588382bbfb
codytseng <codytseng@outlook.com>
2023-08-18 22:03:58 +0800

    refactor: storage service

--- src/nostr/services/storage.service.ts
+++ src/nostr/services/storage.service.ts
@@ -2,42 +2,45 @@ import { Injectable, OnApplicationShutdown } from '@nestjs/common';
 
 @Injectable()
 export class StorageService implements OnApplicationShutdown {
-  private readonly data: Map<string, [any, NodeJS.Timeout]> = new Map();
+  private readonly data = new Map<
+    string,
+    { data: any; timeoutId: NodeJS.Timeout }
+  >();
 
-  async setNx<T = any>(key: string, value: T, ttl = 1000): Promise<boolean> {
+  async setNx<T = any>(key: string, data: T, ttl = 1000): Promise<boolean> {
     const exists = this.data.has(key);
     if (exists) return false;
 
-    await this.set(key, value, ttl);
+    await this.set(key, data, ttl);
     return true;
   }
 
-  async set<T = any>(key: string, value: T, ttl = 1000): Promise<void> {
-    const [, oldTimeoutId] = this.data.get(key) ?? [];
-    if (oldTimeoutId) clearTimeout(oldTimeoutId);
+  async set<T = any>(key: string, data: T, ttl = 1000): Promise<void> {
+    const value = this.data.get(key);
+    if (value) clearTimeout(value.timeoutId);
 
     const timeoutId = setTimeout(async () => {
       await this.del(key);
     }, ttl);
-    this.data.set(key, [value, timeoutId]);
+    this.data.set(key, { data, timeoutId });
   }
 
   async get<T = any>(key: string): Promise<T | undefined> {
-    const [value] = this.data.get(key) ?? [];
-    return value;
+    const value = this.data.get(key);
+    return value ? value.data : undefined;
   }
 
   async del(key: string): Promise<boolean> {
-    const [, timeoutId] = this.data.get(key) ?? [];
-    if (!timeoutId) return false;
+    const value = this.data.get(key);
+    if (!value) return false;
 
-    clearTimeout(timeoutId);
+    clearTimeout(value.timeoutId);
     this.data.delete(key);
     return true;
   }
 
   async onApplicationShutdown() {
-    this.data.forEach(([, timeoutId], key) => {
+    this.data.forEach(({ timeoutId }, key) => {
       clearTimeout(timeoutId);
       this.data.delete(key);
     });
