fb14af58a84a3772c5f337c7d2dfb56918b94abe
codytseng <codytseng@outlook.com>
2023-10-25 23:14:53 +0800

    fix: return appropriate HTTP status (#163)

--- src/common/filters/global-exception.filter.spec.ts
+++ src/common/filters/global-exception.filter.spec.ts
@@ -1,5 +1,5 @@
 import { createMock } from '@golevelup/ts-jest';
-import { ArgumentsHost } from '@nestjs/common';
+import { ArgumentsHost, HttpException } from '@nestjs/common';
 import { PinoLogger } from 'nestjs-pino';
 import { createNoticeResponse } from '../../nostr/utils';
 import { ClientException } from '../exceptions';
@@ -19,25 +19,6 @@ describe('GlobalExceptionFilter', () => {
     );
   });
 
-  it('should send notice response to client when catch error', () => {
-    const errMsg = 'test';
-    const error = new Error(errMsg);
-
-    const mockClientSend = jest.fn();
-    const host = createMock<ArgumentsHost>({
-      getType: () => 'ws',
-      switchToWs: () => ({
-        getClient: () => createMock<WebSocket>({ send: mockClientSend }),
-      }),
-    });
-
-    globalExceptionFilter.catch(error, host);
-
-    expect(mockClientSend).toBeCalledWith(
-      JSON.stringify(createNoticeResponse(errMsg)),
-    );
-  });
-
   it('should not log when catch ClientException', () => {
     const host = createMock<ArgumentsHost>({
       getType: () => 'ws',
@@ -52,24 +33,104 @@ describe('GlobalExceptionFilter', () => {
     expect(loggerErrorMock).not.toBeCalled();
   });
 
-  it('should return error msg when catch error in http', () => {
-    const errMsg = 'test';
-    const error = new Error(errMsg);
+  describe('ws', () => {
+    it('should send notice response to client when catch error', () => {
+      const errMsg = 'test';
+      const error = new Error(errMsg);
 
-    const mockClientSend = jest.fn();
-    const host = createMock<ArgumentsHost>({
-      getType: () => 'http',
-      switchToHttp: () => ({
-        getResponse: () => ({
-          status: () => ({
-            send: mockClientSend,
+      const mockClientSend = jest.fn();
+      const host = createMock<ArgumentsHost>({
+        getType: () => 'ws',
+        switchToWs: () => ({
+          getClient: () => createMock<WebSocket>({ send: mockClientSend }),
+        }),
+      });
+
+      globalExceptionFilter.catch(error, host);
+
+      expect(mockClientSend).toBeCalledWith(
+        JSON.stringify(createNoticeResponse(errMsg)),
+      );
+    });
+  });
+
+  describe('http', () => {
+    it('should return ISE when catch a normal error', () => {
+      const error = new Error('test');
+
+      const mockSend = jest.fn();
+      const mockStatus = jest.fn().mockReturnValue({
+        send: mockSend,
+      });
+      const host = createMock<ArgumentsHost>({
+        getType: () => 'http',
+        switchToHttp: () => ({
+          getResponse: () => ({
+            status: mockStatus,
           }),
         }),
-      }),
+      });
+
+      globalExceptionFilter.catch(error, host);
+
+      expect(mockStatus).toBeCalledWith(500);
+      expect(mockSend).toBeCalledWith('Internal Server Error!');
+    });
+
+    it('should return ISE when catch a 5xx http exception', () => {
+      const error = new HttpException('test', 501);
+
+      const mockSend = jest.fn();
+      const mockStatus = jest.fn().mockReturnValue({
+        send: mockSend,
+      });
+      const host = createMock<ArgumentsHost>({
+        getType: () => 'http',
+        switchToHttp: () => ({
+          getResponse: () => ({
+            status: mockStatus,
+          }),
+        }),
+      });
+
+      globalExceptionFilter.catch(error, host);
+
+      expect(mockStatus).toBeCalledWith(501);
+      expect(mockSend).toBeCalledWith('Internal Server Error!');
+    });
+
+    it('should return error msg when catch a 4xx http exception', () => {
+      const error = new HttpException('test', 404);
+
+      const mockSend = jest.fn();
+      const mockStatus = jest.fn().mockReturnValue({
+        send: mockSend,
+      });
+      const host = createMock<ArgumentsHost>({
+        getType: () => 'http',
+        switchToHttp: () => ({
+          getResponse: () => ({
+            status: mockStatus,
+          }),
+        }),
+      });
+
+      globalExceptionFilter.catch(error, host);
+
+      expect(mockStatus).toBeCalledWith(404);
+      expect(mockSend).toBeCalledWith('test');
+    });
+  });
+
+  it('rpc or others', () => {
+    const error = new Error('test');
+
+    const host = createMock<ArgumentsHost>({
+      getType: () => 'rpc',
     });
 
     globalExceptionFilter.catch(error, host);
 
-    expect(mockClientSend).toBeCalledWith(`Internal Server Error: ${errMsg}`);
+    expect(loggerErrorMock).toBeCalledWith(error);
   });
 });
