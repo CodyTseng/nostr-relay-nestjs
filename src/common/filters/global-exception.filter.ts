b4000f10ad985e9f7469449436765bf72d6fde5f
codytseng <codytseng@outlook.com>
2023-10-25 22:39:04 +0800

    fix: all http requests errors return 500

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -2,6 +2,7 @@ import {
   ArgumentsHost,
   Catch,
   ExceptionFilter,
+  HttpException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
@@ -25,13 +26,35 @@ export class GlobalExceptionFilter implements ExceptionFilter {
       this.logger.error(error);
     }
 
-    if (host.getType() === 'ws') {
-      const wsHost = host.switchToWs();
-      const client = wsHost.getClient<WebSocket>();
-      client.send(JSON.stringify(createNoticeResponse(error.message)));
-    } else if (host.getType() === 'http') {
-      const response = host.switchToHttp().getResponse<Response>();
-      response.status(500).send(`Internal Server Error: ${error.message}`);
+    switch (host.getType()) {
+      case 'ws':
+        this.handleWsException(error, host.switchToWs().getClient<WebSocket>());
+        break;
+      case 'http':
+        this.handleHttpException(
+          error,
+          host.switchToHttp().getResponse<Response>(),
+        );
+        break;
+      default:
+        break;
     }
+  }
+
+  private handleWsException(error: Error, client: WebSocket) {
+    client.send(JSON.stringify(createNoticeResponse(error.message)));
+  }
+
+  private handleHttpException(error: Error, response: Response) {
+    if (!(error instanceof HttpException)) {
+      return response.status(500).send('Internal Server Error!');
+    }
+
+    const status = error.getStatus();
+    if (status >= 500 && status < 600) {
+      return response.status(status).send('Internal Server Error!');
+    }
+
+    return response.status(status).send(error.message);
   }
 }
