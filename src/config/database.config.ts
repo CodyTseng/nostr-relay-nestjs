7ac926883af2cbe2e401f9b9042bd6bc0161fe66
codytseng <codytseng98@gmail.com>
2024-05-28 22:40:04 +0800

    feat: add DATABASE_MAX_CONNECTIONS environment variable

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,6 +3,7 @@ import { Environment } from './environment';
 export function databaseConfig(env: Environment) {
   return {
     url: env.DATABASE_URL,
+    maxConnections: env.DATABASE_MAX_CONNECTIONS ?? 50,
   };
 }
 export type DatabaseConfig = ReturnType<typeof databaseConfig>;
