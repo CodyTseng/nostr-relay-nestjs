35e0ff3300f1bc82c2c71640406ea773ac86b704
codytseng <codytseng98@gmail.com>
2024-10-04 00:06:46 +0800

    feat: update validation for name field

--- src/modules/nip-05/schemas/register-nip-05.schema.ts
+++ src/modules/nip-05/schemas/register-nip-05.schema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
 export const RegisterNip05Schema = z.object({
-  name: z.string().max(20),
+  name: z.string().regex(/^[a-zA-Z0-9_]{1,20}$/),
   pubkey: z.string().regex(/^[0-9a-f]{64}$/),
 });
